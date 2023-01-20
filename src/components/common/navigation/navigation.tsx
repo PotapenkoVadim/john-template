209550b061b9be140fef61940efd0b65703badd3
PotapenkoVadim <potapenko.v00@mail.ru>
2023-01-20 11:39:41 +0300

    fix: fix active link

--- src/components/common/navigation/navigation.tsx
+++ src/components/common/navigation/navigation.tsx
@@ -1,6 +1,7 @@
 import Link from '@/components/ui-kit/link/link';
 import { NavigationItem } from '@/interface';
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/router';
 import styles from './navigation.module.scss';
 
 const cx = classNames.bind(styles);
@@ -12,13 +13,19 @@ export default function Navigation({
   routes: Array<NavigationItem>;
   className?: string;
 }): JSX.Element {
+  const router = useRouter();
+
   return (
     <nav className={cx(['navigation', className])}>
       {routes.length > 0 &&
         routes.map((route) => (
           <Link
+            className={cx({
+              navigation__link_active: router.asPath === route.link
+            })}
             key={route.id}
-            href={route.link}>
+            href={route.link}
+          >
             {route.node}
           </Link>
         ))}
